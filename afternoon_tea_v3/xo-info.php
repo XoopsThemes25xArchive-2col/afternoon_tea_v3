3920503fc083bab91654e0a63839116bc8b27534
mambax7 <mambax7@gmail.com>
2015-06-06 04:57:03 -0400

    Original files

---
+++ afternoon_tea_v3/xo-info.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code 
+ which is considered copyrighted (c) material of the original comment or credit authors.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * XOOPS default theme configuration file
+ *
+ * Not used yet, for forward compat
+ *
+ * @copyright   The Xoops project http://www.xoops.org/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @author      Taiwen Jiang <phppp@users.sourceforge.net>
+ * @since       2.3
+ * @version     $Id: xo-info.php 2793 2009-02-10 19:49:25Z dugris $
+ */
+
+
+return array(
+    // Types of language constants
+    "languages"         => array("main", "admin"),
+
+    "copyright"         => "&copy; Oceannet.jp",
+    "name"              => "Afternoon Tea v3",
+    "description"       => "XOOPS 2.0.1x and 2.4.x, 2 columns, 740px",
+    "version"           => "2",
+    "format"            => "Default theme",
+    "author"            => "Oceannet.jp",
+    "demo"              => "http://www.babylonbyxoops.net",
+    "url"               => "http://www.babylonbyxoops.net",
+    "download"          => "http://www.babylonbyxoops.net",
+    "W3C"               => "",
+    "licence"           => "GPL",
+    "thumbnail"         => "",
+    "screenshot"        => "",
+);
+?>
